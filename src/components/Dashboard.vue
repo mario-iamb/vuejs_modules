cc94838de147e9b843104ceb7ab48bd515e41b95
MarioDC <mario@design-culture.co.uk>
2018-08-20 17:12:50 +0100

    finished Employee app

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,22 +1,73 @@
 <template>
   <div id="dashboard">
-      <h3>Dashboard</h3>
+    <h3>Dashboard</h3>
+    <ul class="collection">
+        <li class="collection-header"><h4>All Employees</h4></li>
+        <li v-for='employee in employees' v-bind:key="employee.id">
+            {{employee.dept}} : {{employee.employee_id}} : {{employee.name}}
+            <router-link class="edit-employee" v-bind:to="{name:'view-employee', params:{employee_id: employee.employee_id}}" >View</router-link>
+        </li>
+    </ul>
+
+    <div class="fixed-action-btn">
+        <router-link to="/new" class="add-new-btn">ADD NEW EMPLOYEE</router-link>
+    </div>
   </div>
 </template>
 
 
 <script>
+
+    import db from './firebaseInit'
     export default {
         name: 'dashboard',
         data(){
             return{
-
+                employees:[]
             }
+        },
+        created() {
+            db.collection('employees').get().then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    //console.log(doc.data());
+                    const data = {
+                        'id': doc.id,
+                        'employee_id' : doc.data().employee_id,
+                        'name': doc.data().name,
+                        'dept': doc.data().dept,
+                        'position': doc.data().position
+                    }
+                    this.employees.push(data)
+                })
+            })
         }
     }
 </script>
 
 
-<style scoped>
+<style lang="scss" scoped>
+    .add-new-btn {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        font-weight: bold;
+        text-decoration: none;
+        font-size: 12px;
+        color: red;
+        transition: color 0.2s ease-in;
 
+        &:hover {
+            color: blue;
+            transition: color 0.2s ease-in;
+        }
+    }
+
+    li {
+        list-style-type: none;
+        //border: 1px dotted grey;
+    }
+
+    .edit-employee {
+        font-size: 11px;
+        color: black;
+        padding-left: 30px;
+    }
 </style>
--- src/components/NewEmployee.vue
+++ src/components/NewEmployee.vue
@@ -1,16 +1,64 @@
 <template>
   <div id="new-employee">
-      <h3>New Emplyee</h3>
+    <h3>Add New Emplyee</h3>
+    <form @submit.prevent="saveEmployee">
+        <div class="row">
+            <div class="input-field">
+                <label for="">Employee ID </label>
+                <input type="text" v-model="employee_id" requered>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field">
+                <label for="">Name </label>
+                <input type="text" v-model="name" requered>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field">
+                <label for="">Department </label>
+                <input type="text" v-model="dept" requered>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="input-field">
+                <label for="">Position </label>
+                <input type="text" v-model="position" requered>
+            </div>
+        </div>
+
+        <button type="submit" class="btn">Submit</button>
+        <router-link to='/Dashboard' >Cancel</router-link>
+    </form>
   </div>
 </template>
 
 
 <script>
+    import db from './firebaseInit'
     export default {
         name: 'new-employee',
         data(){
             return{
-                
+                employee_id: null,
+                name: null,
+                dept: null,
+                position: null
+            }
+        },
+        methods: {
+            saveEmployee () {
+                db.collection('employees').add({
+                    employee_id: this.employee_id,
+                    name: this.name,
+                    dept: this.dept,
+                    position: this.position
+                })
+                .then(docRef => this.$router.push('/Dashboard'))
+                .catch(error => console.log(err))
             }
         }
     }
